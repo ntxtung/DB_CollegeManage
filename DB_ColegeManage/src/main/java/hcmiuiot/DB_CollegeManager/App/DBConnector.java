4e69d62e0c60bf00ab7ff0a851b0356ef1b4a61e
Nguyễn Thanh Xuân Tùng <xuantung.1998@gmail.com>
2018-05-11 12:11:04 +0700

    Initialize comit

---
+++ DB_ColegeManage/src/main/java/hcmiuiot/DB_CollegeManager/App/DBConnector.java
@@ -0,0 +1,26 @@
+package hcmiuiot.DB_CollegeManager.App;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DBConnector {
+
+	public Connection connectDB() {
+		/// Driver
+		try {
+			String url = "jdbc:mysql://db.hcmiuiot.club/ititiu16144";
+			String username = "ititiu16144";
+			String password = "Trung@1998";
+
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection connect = DriverManager.getConnection(url, username, password);
+			return connect;
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			Logger.getLogger(DBConnector.class.getName()).log(Level.SEVERE, null, e);
+		}
+		return null;
+	}
+}
