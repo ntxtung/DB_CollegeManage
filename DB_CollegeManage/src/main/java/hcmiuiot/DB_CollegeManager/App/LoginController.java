4798d25af4b988b5a0d5675204744681cf1ff771
Nguyễn Phan Hùng Thuận <hungthuanmk@gmail.com>
2018-05-17 23:47:13 +0700

    TIDY UP

--- DB_CollegeManage/src/main/java/hcmiuiot/DB_CollegeManager/App/LoginController.java
+++ DB_CollegeManage/src/main/java/hcmiuiot/DB_CollegeManager/App/LoginController.java
@@ -2,13 +2,9 @@ package hcmiuiot.DB_CollegeManager.App;
 
 import java.io.IOException;
 import java.net.URL;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import org.apache.commons.codec.digest.DigestUtils;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
@@ -18,7 +14,6 @@ import com.jfoenix.validation.RequiredFieldValidator;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import hcmiuiot.DB_CollegeManager.DatabaseHandler.DbHandler;
-import javafx.animation.PauseTransition;
 import javafx.application.Platform;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
@@ -28,12 +23,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Label;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 public class LoginController implements Initializable{
 	private Label label;
